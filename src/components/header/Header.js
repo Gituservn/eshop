afe29fe90396da472d74274ae82559eaf7e7a904
Evgeniy <zhrkacomua@gmail.com>
2022-11-16 23:28:46 +0200

    add: responsive menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,56 @@
 import React from "react";
-import './Header.module.scss';
+import styles from './Header.module.scss';
 import {Link} from "react-router-dom";
+import {FaShoppingCart} from 'react-icons/fa';
+import {HiMenuAlt3} from "react-icons/hi";
 
-const spanStyle={
-    color:'orangered'
-}
+const spanStyle = {
+    color: 'orangered'
+};
+
+const logo = (
+    <div className="logo">
+        <Link to="/">
+            <h2>
+                <span style={spanStyle}>W</span>illow.
+            </h2>
+        </Link>
+    </div>
+);
+
+const cart = (
+    <span className={styles.cart}>
+                        <Link to="/cart">Кошик <FaShoppingCart
+                            size={20}/> <p>0</p> </Link>
+                    </span>);
+
+
 const Header = () => {
     return (
         <header>
-            <div className="header">
-                <div className="logo">
-                    <Link to="/">
-                        <h2>
-                            <span style={spanStyle} >Willow</span>.
-                        </h2>
-
-                    </Link>
+            <div className={styles.header}>
+                {logo}
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Головна</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">зв'яжіться з нами</Link>
+                        </li>
+                    </ul>
+                    <div className={styles["header-right"]}>
+                        <span className={styles.links}>
+                            <Link to="/login">Увійти</Link>
+                            <Link to="/register">Реєстрація</Link>
+                            <Link to="/orderHistory">Мої замовлення</Link>
+                        </span>
+                        {cart}
+                    </div>
+                </nav>
+                <div className={styles['menu-icon']}>
+                    {cart}
+                    <HiMenuAlt3 size={40}/>
                 </div>
             </div>
         </header>
