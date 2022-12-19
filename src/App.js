fcec3bd14cfa8abe983de3a0a19748f1e7f0e84c
Evgeniy <zhrkacomua@gmail.com>
2022-12-20 00:03:17 +0200

    add: Added single product page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Home,Contact,Login,Register,Reset,Admin} from './pages/index'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AdminOnlyRoute from "./components/adminOnlyRoute/adminOnlyRoute";
+import ProductDetails from "./components/products/productDetails/ProductDetails";
 
 
 
@@ -23,10 +24,13 @@ function App() {
                     <Route path="/register" element={<Register/>}/>
                     <Route path="/reset" element={<Reset/>}/>
 
+
                     <Route path="/admin/*" element={
                         <AdminOnlyRoute>
                             <Admin/>
                         </AdminOnlyRoute>}/>
+
+                    <Route path="/product-details/:id" element={<ProductDetails/>}/>
                 </Routes>
                 <Footer/>
             </BrowserRouter>
--- src/components/products/productDetails/ProductDetails.js
+++ src/components/products/productDetails/ProductDetails.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {doc, getDoc} from "firebase/firestore";
+import {db} from "../../../firebase/Config";
 
 const ProductDetails = () => {
+    const {id}=useParams()
+    console.log(id)
+    const [product, setProduct] = useState(null);
+    useEffect(()=>{
+        getProduct()
+    },[])
+
+    //https://firebase.google.com/docs/firestore/query-data/get-data
+    const getProduct = async () => {
+        console.log('get product')
+        const docRef = doc(db, "products", id);
+        const docSnap  = await  getDoc(docRef);
+
+        if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }
+
+
+
     return (
-        <div>ProductDetails</div>
+        <div><p>{id}</p></div>
     );
 }
+
+
+
 
 export default ProductDetails;
