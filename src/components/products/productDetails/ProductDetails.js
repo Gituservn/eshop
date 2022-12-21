eab5c97da7b3351f9c028de007021e7f488e0bb8
Evgeniy <zhrkacomua@gmail.com>
2022-12-21 21:19:31 +0200

    add: наробив хуйні кароче

--- src/components/products/productDetails/ProductDetails.js
+++ src/components/products/productDetails/ProductDetails.js
@@ -1,4 +1,3 @@
-
 import React, {useEffect, useState} from 'react';
 import {Link, useParams} from "react-router-dom";
 import {doc, getDoc} from "firebase/firestore";
@@ -64,7 +63,36 @@ const ProductDetails = () => {
                                 <p>
                                     <b>Виробник:</b>{product.brand}
                                 </p>
+                                <form style={{display:'flex'}}>
+                                    {product.pillowSize40 ?
+                                        <>
+                                            <label htmlFor="">40/70</label>
+                                            <input type="radio"/>
+                                        </> : null}
+                                    {product.pillowSize50 ?
+                                        <>
+                                            <label htmlFor="">50/70</label>
+                                            <input type="radio"/>
+                                        </> : null}
 
+                                    {product.pillowSize70 ?
+                                        <>
+                                            <label htmlFor="">70/70</label>
+                                            <input type="radio"/>
+                                        </>
+                                        : null}
+                                    {product.pillowSize40plus ?
+                                        <>
+                                            <label htmlFor="">50/70 +(борт)5см</label>
+                                            <input type="radio"/>
+                                        </> : null}
+
+                                    {product.pillowSize50plus ?
+                                        <>
+                                            <label htmlFor="">40/60 +(борт)5см</label>
+                                            <input type="radio"/>
+                                        </> : null}
+                                </form>
 
                                 <div className={styles.count}>
                                     <button onClick={() => setCount(count - 1)} className="--btn">-</button>
@@ -79,7 +107,7 @@ const ProductDetails = () => {
                                 <Accordion allowZeroExpanded>
                                     <AccordionItem>
                                         <AccordionItemHeading>
-                                            <AccordionItemButton style={{fontSize:'20px'}}>
+                                            <AccordionItemButton style={{fontSize: '20px'}}>
                                                 Детальніше
                                             </AccordionItemButton>
                                         </AccordionItemHeading>
